7f3840186b78c1ef32df4cdc871b5de836521bbd
SIDDHANT PRATAP <83108673+siddhantpratap101@users.noreply.github.com>
2022-05-25 02:05:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+        let dislay = document.getElementById("display");
+        let mybtn = document.querySelectorAll("button");
+        let currentdisplay = "";
+
+        for (let item of mybtn) {
+            item.addEventListener("click", (e) => {
+                mybtntext = e.target.innerText;
+                // console.log(mybtntext);
+
+                if (mybtntext == "C") {
+                    currentdisplay = "";
+                    display.value = currentdisplay;
+
+                } else if (mybtntext == "=") {
+                    display.value = eval(currentdisplay);
+                    // 2+3*5/500
+
+                } else {
+                    currentdisplay = currentdisplay + mybtntext;
+                    display.value = currentdisplay;
+                }
+            })
+        }
